75ede26fdee5f7cec8aee9272ae1395f6d6aab79
Elben Shira <elbenshira@gmail.com>
2015-06-02 16:42:08 +0800

    Applicative and Functor exercises.

--- exercises/Applicative.hs
+++ exercises/Applicative.hs
@@ -6,16 +6,31 @@ module Applicative where
 -- The lecture is from 2015 course, lecture week 8, Monads Part II.
 --
 -- http://www.seas.upenn.edu/~cis194/lectures/08-monads-ii.html
+--
+-- http://learnyouahaskell.com/functors-applicative-functors-and-monoids
+-- https://wiki.haskell.org/Typeclassopedia#Applicative
+-- http://en.wikibooks.org/wiki/Haskell/Applicative_Functors
+
+-- To understand Applicative functors, first understand Functors.
+
+
+-- Compare the applicative functor type class with functor:
+--
+--
+-- class Functor f where
+--   fmap :: (a -> b) -> f a -> f b
 
 class Functor f => Applicative f where
   pure :: a -> f a
 
   -- Pronounced "apply", or "ap" for short
   (<*>)  :: f (a -> b) -> f a -> f b
+-- Functor:
+--  fmap ::   (a -> b) -> f a -> f b
 
-  -- Also get:
-  (<$>) :: (a -> b) -> f a -> f b
-  (<$>) = fmap
+-- Also get the infix <$>, which is an alias for fmap.
+(<$>) :: Functor f => (a -> b) -> f a -> f b
+(<$>) = fmap
 
 instance Applicative [] where
   pure a = [a]
@@ -31,7 +46,23 @@ instance Applicative [] where
 -- Examples:
 --
 -- pure 4 :: [Int]
--- [id, (+1)] <*> [1,2,3]
+-- [id, (+100)] <*> [1,2,3]
+-- => [1, 2, 3, 101, 102, 103]
+--
+-- :t pure (+3)
+-- => (Applicative f, Num a) => f (a -> a)
+--
+-- pure (+3) <*> Just 1
+-- => Just 4
+--
+-- pure (+3) <*> [1, 2, 3]
+-- => [4, 5, 6]
+--
+-- (++) <$> ["hello", "world"] <*> [".", "!"]
+-- => ["hello.","hello!","world.","world!"]
+-- In above, the first part (++) <$> ["hello", "world"] turns into partially
+-- applied functions: ["hello" ++, "world" ++]. This is then passed into the <*>
+-- part.
 
 instance Applicative Maybe where
   pure a = Just a
@@ -39,6 +70,9 @@ instance Applicative Maybe where
   (<*>) Nothing _ = Nothing
   (<*>) _ Nothing = Nothing
   (<*>) (Just f) (Just a) = Just $ f a
+
+  -- Alternative implementation using fmap:
+  -- (<*>) (Just f) ma = fmap f ma
 
   -- Alternative implementation using maybe monad:
   -- (<*>) h fa = do
@@ -51,6 +85,59 @@ instance Applicative Maybe where
 -- pure 4 :: Maybe Int
 -- Just (+1) <*> pure 4
 -- Just (+1) <*> Just 4
+--
+-- Nothing <*> Just 3
+-- => Nothing
+
+instance Applicative IO where
+  pure = return
+
+  -- (<*>)  :: f (a -> b) -> f a -> f b
+  f <*> fa = do
+    f' <- f
+    a <- fa
+    return $ f' a
+
+-- Examples of Applicative IO:
+--
+-- main = do
+--   twoLines <- (++) <$> getLine <*> getLine
+--   print twoLines
+
+--------------------------------------------------
+-- When Applicative is more useful than Functors.
+--------------------------------------------------
+--
+-- With a functor, you can only fmap a function that operates on the value
+-- inside the context. You also can't get the value out of the context in a
+-- general way:
+--
+-- fmap :: (a -> b) -> f a -> f b
+--
+-- (+) `fmap` Just 1. <--- This works (we get a partial function (+1) inside a
+-- Just), but what if we want to continue doing things to the result of it, like
+-- give it the second argument? We can't do another `fmap` like this:
+-- (+) `fmap` Just 1 `fmap` Just 3 => 4. Won't work!! We have to pattern match
+-- explicitly on the Maybe--it's not general for all data types.
+--
+-- Applicative, however, gives us the power to do this!
+--
+-- let x = Just 1
+-- let y = Just 3
+-- (+) <$> x <*> y
+-- => Just 4
+--
+-- let x = Just 1
+-- let y = Nothing
+-- (+) <$> x <*> y
+-- => Nothing
+--
+--
+-- We can also do:
+-- pure (+) <*> Just 1 <*> Just 3
+--
+-- This shows that (pure (+) Just 1) is equivalent to ((+) <$> Just 1). In fact,
+-- this is one of the Applicative laws.
 
 type Name = String
 data Employee = Employee { name :: Name, phone :: String }
@@ -68,3 +155,4 @@ data Employee = Employee { name :: Name, phone :: String }
 -- 1.    (Employee <$> Just "Elben") <*> Just "55555555"
 -- 2. (fmap Employee (Just "Elben")) <*> Just "55555555"
 -- 3.      (Just (Employee "Elben")) <*> Just "55555555"
+
--- exercises/Functor.hs
+++ exercises/Functor.hs
@@ -1,5 +1,9 @@
 module Functor where
 
+-- https://wiki.haskell.org/Typeclassopedia#Functor
+-- http://learnyouahaskell.com/functors-applicative-functors-and-monoids
+-- http://www.seas.upenn.edu/~cis194/lectures/04-typeclasses.html
+
 -- Hide Prelude's functors
 import Prelude hiding (fmap, Functor)
 
@@ -23,5 +27,114 @@ instance Functor Maybe where
 -- Examples:
 --
 -- fmap (+1) (Just 3)
+-- fmap (+1) Nothing
 
+-- If a tuple has type constructor (,), then ((,) a) is a tuple where the first
+-- element is of type a. A Functor only accepts one type variable, so we must
+-- partially apply the tuple as ((,) a), so that the first element is of type a.
+instance Functor ((,) a) where
+  fmap f (a, b) = (a, f b)
+
+-- Examples:
+--
+-- fmap (++ "3") (1, "2")
+-- => (1, "23") :: Num t => (t, String)
+
+---------------------
+-- "Lifting"
+---------------------
+--
+-- We can think of Functors also as "lifting" a function (a -> b) from the
+-- normal world into the "f" world:
+--
+-- fmap :: (a -> b) -> f a -> f b
+--
+-- But when curried, we can think of it as:
+--
+-- fmap :: (a -> b) -> (f a -> f b)
+--
+-- So the function (a -> b) is "lifted" into the "f" world, producing (f a -> f b).
+
+-- Here, -> is also treated as a type constructor that is partially applied. We
+-- know the argument is of type `r`, but the return value us unknown in the
+-- constructed type. Again, we must partially apply for Functor since Functor
+-- only accepts one type var.
+instance Functor ((->) r) where
+  -- f :: (a -> b)
+  -- g :: (r -> a)
+  -- fmap's type tells us this must return function of type (r -> b):
+  -- fmap :: (a -> b) -> (r -> a) -> (r -> b)
+  fmap f g = \r -> f (g r)
+  -- This is equivalent to:
+  -- fmap f g = f . g.
+  -- Which is function composition! So a function's Functor acts like function
+  -- composition.
+
+-- Examples:
+-- (fmap (+1) (*3)) 10
+-- => 31
+-- ((+1) . (*3)) 10
+-- => 31
+
+-- IO is also a Functor. In essense, to fmap over an IO type is to:
+--
+-- 1. Do the IO that return a value, then
+-- 2. Call f on that returned value, returning another value.
+instance Functor IO where
+  -- fmap :: (a -> b) -> IO a -> IO b
+  fmap f io = do
+    a <- io
+    return $ f a
+
+-- Examples:
+-- fmap reverse getLine
+--
+-- 1. Get line from user
+-- 2. Reverse the string from user
+
+-------------------
+-- How to think about Functors
+-------------------
+--
+-- The easy way to think about functors is to think of them as 'mappable'. You
+-- can map over lists, you can 'map' over Maybe and Tuples.
+--
+-- The more general abstraction is thinking of Functors as a "computational
+-- context". For example, with the IO and (r -> ) functors, when we 'fmap f o'
+-- over them, we are doing some computation 'f' on the context of 'o'.
+--
+-- For IO, we can think of 'o' as the context (namely, the IO that's happening)
+-- that 'f' is being applied on. For (r -> ), consider the example `(fmap (+1)
+-- (*3)) 10`. We are doing a computation (+1) in the 'context' of (*3).
+
+-------------------
+-- Functor Laws
+-------------------
+-- Useful functors hold two laws to make sure that `fmap g` does not change the
+-- structure of the container, but only the elements inside. (e.g. trees, lists)
+--
+-- (1) fmap id = id
+-- (2) fmap (g . h) = (fmap g) . (fmap h)
+--
+-- (1) shows that mapping id over the elements have no effect (i.e. same as
+-- calling id on the container). This also shows that fmap didn't do any 'extra'
+-- secret to our functor work. E.g. this is an invalid implementation:
+-- fmap f num = f (num + 1), since ((fmap id) 0) != id 0
+--
+-- (2) shows that running two fmaps with two functions on container is the same
+-- as running fmap once using the composed function (g . h).
+--
+-- This makes implementations that modify the structure invalid:
+--
+-- fmap f list = map f (reverse list), since:
+--
+-- fmap ((+1) . (*3)) [1, 2, 3] != ((fmap (+1)) . (fmap (*3))) [1, 2, 3]
+--
+invalidFmap :: (a -> b) -> [a] -> [b]
+invalidFmap f list = map f (reverse list)
+-- invalidFmap ((+1) . (*3)) [1, 2, 3]
+-- => [4, 7, 10]
+-- ((invalidFmap (+1)) . (invalidFmap (*3))) [1, 2, 3]
+-- => [10, 7, 4]
+--
 
